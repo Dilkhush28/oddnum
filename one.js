be2a2ad1a5c09b12b4f4a6140f6c44c7033a1362
Dilkhush28 <dilkhushiimt@gmail.com>
2024-07-25 18:02:22 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,73 @@
+// white loop
+// let i=1;
+// while (i<=5){
+//     console.log("hello")
+//     i++;
+// }
+
+
+
+//do while
+// let i=5;
+// do {
+
+//     console.log("chai aur code")
+//     i++;
+// } while (i<=4);
+
+
+// for-of-loop
+// let str = "ApnaCollage";
+
+// for(let i of str){
+//     console.log("i=",i)
+// }
+
+// let btr = "chai aur code"
+// for (const i of btr) {
+//     console.log("i=",i)
+// }
+
+// let star = "1234567890875657"
+// for (const num of star) {
+//     console.log(num)
+// }
+
+
+// quetion
+
+// even number -1st
+
+//  let num;
+// for(i=1; i<=100; i++){
+// if(i%2 === 0){
+//     console.log(i)
+// }
+
+// }
+
+// odd num -2nd
+// for(i=1; i<=10; i++){
+//     if(i%2 === 1){
+//         console.log(i)
+//     }
+// }
+
+
+
+//3rd
+
+// let gameNum = 25;
+// let userNum = prompt("Guess the number");
+
+// while (gameNum != userNum) {
+//     userNum = prompt("You entered wrong number , plz try again");
+   
+// }
+// console.log("congratulation you entered the rigth number")
+
+// 4th
+
+ let userName = prompt("Enter the Full name")
+let userfullname = "@".concat.userName.concat.userName.length
+// concat mean +,
